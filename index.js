46073806dfe4f2e359630e13f6612f228e7f6332
hussain2023 <iamhussainali2@gmail.com>
2022-10-27 15:31:45 +0530

    successfully running the server

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const Port = process.env.PORT || 5000;
+
+const courses = require("./data/courses.json");
+
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(Port, () => {
+  console.log("server is running", Port);
+});
+
+module.exports = app;
