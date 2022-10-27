cadf1ac14eb98f80b8fb8f3d5a1c6efe008b03cc
hussain2023 <iamhussainali2@gmail.com>
2022-10-27 23:21:31 +0530

    'final'

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ app.use(cors());
 const Port = process.env.PORT || 5000;
 
 const courses = require("./data/courses.json");
-
 app.get("/", (req, res) => {
   res.send("server is running");
 });
 
 app.get("/courses", (req, res) => {
   res.send(courses);
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const singleCourse =
+    courses.find((course) => course.id === id) || "No Course found";
+  res.send(singleCourse);
 });
 
 app.listen(Port, () => {
